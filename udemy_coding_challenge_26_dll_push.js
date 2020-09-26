f825aad7f4af323f2f427a4c7968e190748cfd18
Crew Spence <csspence123@gmail.com>
2020-09-26 06:00:41 -0500

    Created function stub for toy problem.

---
+++ udemy_coding_challenge_26_dll_push.js
@@ -0,0 +1,27 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+
+Implement the following on a doubly linked list.
+
+push
+
+This function should accept a value add a node to the end of a doubly linked list with that given value.  It should return the doubly linked list.
+*/
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null;      
+        this.prev = null;      
+    }
+}
+
+class DoublyLinkedList{
+    constructor(val){
+        this.val = val
+        this.next = null;      
+    }
+    push(){
+    }
+    
+}
